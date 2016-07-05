ccc39d6ea4144de1c127be66957de5b4d60e3e85
Chris Coppola <MyNameReallySux@gmail.com>
2016-07-04 22:51:20 -0400

    Updated gitignore

--- python-test/core.py
+++ python-test/core.py
@@ -7,6 +7,6 @@ from StyleExtractor.extractor import extract_style
 def main():
     page = requests.get('http://www.robinwallack.com/' + input('Get Site, http://www.robinwallack.com/'))
     page = extract_style(page)
-    page = combine_classes()
+    page = combine_classes(page)
     
 main();
